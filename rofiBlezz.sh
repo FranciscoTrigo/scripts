3d207061ccb7cd159aa59bbc7e3e7ed16a48b823
yamifrankc <code@yamifrankc.com>
2023-05-16 13:16:34 -0500

    New scripts

---
+++ rofiBlezz.sh
@@ -0,0 +1,5 @@
+## Just a script to call Rofi Blezz easily
+# Visit https://github.com/davatorium/rofi-blezzhttps://github.com/davatorium/rofi-blezz
+# and https://github.com/Blezzing/blezz for more information
+#
+rofi -show blezz -modi blezz -auto-select -matching normal -blezz-directory ~/.config/blezz
